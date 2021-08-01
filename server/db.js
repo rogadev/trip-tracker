5d4bb528c32b50f83efc7532668c65bdea63975f
vonroga <ryan@mossaway.ca>
2021-08-01 11:33:30 -0700

    Update db.js
    Now able to add and find passengers to/in the DB.

--- server/db.js
+++ server/db.js
@@ -1,3 +1,4 @@
+// --- SETUP --- //
 const { MongoClient } = require('mongodb');
 
 function createURI() {
@@ -10,20 +11,25 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-async function main() {
+// --- END SETUP --- //
+
+async function test() {
   try {
     await client.connect();
-    console.log('Connected to MongoDB');
-    await listDatabases();
+
+    const result = await findOnePassenger('name', 'betty');
+    console.log(result);
   } catch (err) {
     console.log(err);
   } finally {
     await client.close();
   }
 }
+test().catch(console.error);
 
-main().catch(console.error);
+// --- LIST & CREATE --- //
 
+/* List all the DB's in the collection. */
 async function listDatabases() {
   const dbList = await client.db().admin().listDatabases();
   console.log('Databases:');
@@ -32,10 +38,34 @@ async function listDatabases() {
   });
 }
 
+/* Create a new passenger in the DB. (working) */
 async function createPassenger(newPassenger) {
-  const result = client
+  // TODO - Validate new passenger object...
+  const result = await client
     .db('trip_data')
     .collection('passengers')
     .insertOne(newPassenger);
   console.log('New passenger added and given the ID: ' + result.insertedId);
+  return {
+    id: result.insertedId.toString(),
+    success: result.acknowledged || false,
+  };
+}
+
+// --- WRITE --- //
+
+/* Finds a passenger with the given field type. */
+async function findOnePassenger(key, value) {
+  const result = await client
+    .db('trip_data')
+    .collection('passengers')
+    .findOne({ [key]: value });
+
+  if (result) {
+    console.log('Found.');
+    return result;
+  } else {
+    console.log('Not found.');
+    return null;
+  }
 }
