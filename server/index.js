ffc53a639e20118fedc9fb966b180edc86c42abd
vonroga <ryan@mossaway.ca>
2021-08-10 22:01:04 -0700

    Testing Push
    GitHub wasn't allowing commits from GH Desktop.

--- server/index.js
+++ server/index.js
@@ -1,27 +1,19 @@
+// Import dotenv
+require('dotenv').config();
+// Import & setup express
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
-
-/* Definitions */
-const port = process.env.PORT || 5000;
-const domain = `localhost:${port}`;
-
-/* Port listening */
-app.listen(port, () => {
-  console.log(`Server running on ${domain}`);
-});
-
-/* Middlewares */
+const port = process.env.PORT || 3000;
+const domain = process.env.DOMAIN || 'localhost';
+const api = require('./api');
+// Import cors
+const cors = require('cors');
+// Setup Middlewares
 app.use(cors());
 app.use(express.json());
-
-/* Endpoints */
-// const passengers = require('./routes/api/passengers');
-// const login = require('./routes/api/v1/login/login');
-// const newUser = require('./routes/api/v1/new/new');
-// app.use('/api/passengers', passengers);
-// app.use('/api/v1/login', login);
-// app.use('/api/v1/new', newUser);
-const test = require('./db/index');
-app.use('/test', test);
+// Start server
+app.listen(port, domain, () => {
+  `Server running on port ${port}`;
+});
+// API Endpoints
+app.use('/api', api);
--- server/routes/api/v2/test.js
+++
@@ -1,1 +0,0 @@
-//mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@trip-tracker.luvm1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
